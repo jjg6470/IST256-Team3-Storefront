ad4f7cdc5630ab78fdc5e1425ff578368a6618b6
Thejoshuajose <joshuajose3@icloud.com>
2024-11-21 14:10:03 -0500

    Changes

--- angular.js
+++ angular.js
@@ -13,3 +13,21 @@ angular.module('billingApp', []).controller('ShippingController', function ($sco
         }
     };
 });
+
+angular.module('billingApp', []).controller('BillingController', function ($scope) {
+        $scope.showBillingInfo = false;
+
+        $scope.displayBillingInfo = function () {
+            $scope.billingInfoJSON = {
+                billingAddress: $scope.billingAddress,
+                billingCity: $scope.billingCity,
+                billingState: $scope.billingState,
+                billingZip: $scope.billingZip,
+                cardNumber: $scope.cardNumber,
+                cardHolder: $scope.cardHolder,
+                expirationDate: $scope.expirationDate,
+                cvv: $scope.cvv
+            };
+            $scope.showBillingInfo = true;
+        };
+    });
